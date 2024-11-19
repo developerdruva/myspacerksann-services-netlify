a438d161e864b60d7285af7583ccd2a1a0469296
Rajesh <developer.rajeshnuz@gmail.com>
2024-11-19 19:18:42 +0530

    .

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,26 +3,26 @@ import ServerlessHttp from "serverless-http";
 // const express = require('express');
 // const router = require('express/lib/router');   
 // const ServerlessHttp = require('serverless-http');
-const dotenv = require('dotenv');
-import dotenv from 'dotenv';
-// import DB from ''
-const DB = require('./db/mongo/mongoConnection')
-dotenv.config();
+// const dotenv = require('dotenv');
+// import dotenv from 'dotenv';
+// // import DB from ''
+// const DB = require('./db/mongo/mongoConnection')
+// dotenv.config();
 
-DB.connectToDB();
-const PORT = process.env.PORT || 8080
+// DB.connectToDB();
+// const PORT = process.env.PORT || 8080
 
-const cors = require('cors');
+// const cors = require('cors');
 
-const MyspaceRoutes = require('./routes/myspace.routes');
-const SampleDataRoutes = require('./routes/sample.data.routes');
-const UserAccountRoutes = require('./routes/account.user.routes');
-// const todoListRoutes = require('./routes/todolist.user.routes');
+// const MyspaceRoutes = require('./routes/myspace.routes');
+// const SampleDataRoutes = require('./routes/sample.data.routes');
+// const UserAccountRoutes = require('./routes/account.user.routes');
+// // const todoListRoutes = require('./routes/todolist.user.routes');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 const router = Router();
 
@@ -40,9 +40,9 @@ router.get('/sampleroute', (webReq, webRes)=> {
         message:'routes are working.'
     })
 })
-router.use(MyspaceRoutes);
-router.use(SampleDataRoutes);
-router.use(UserAccountRoutes);
+// router.use(MyspaceRoutes);
+// router.use(SampleDataRoutes);
+// router.use(UserAccountRoutes);
 
 app.use('/api/', router);
 
